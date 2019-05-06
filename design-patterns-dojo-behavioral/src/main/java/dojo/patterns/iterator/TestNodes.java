89743a9feee309dbac5f13fad87660fdb1526795
Kornél Simon <kokog.private@gmail.com>
2019-05-06 12:07:11 +0200

    Iterátor feladat módosítása

--- design-patterns-dojo-behavioral/src/main/java/dojo/patterns/iterator/TestNodes.java
+++ design-patterns-dojo-behavioral/src/main/java/dojo/patterns/iterator/TestNodes.java
@@ -3,8 +3,16 @@ package dojo.patterns.iterator;
 import java.util.Iterator;
 
 import dojo.patterns.testresults.TestNode;
+import dojo.patterns.testresults.TestRun;
 
 public class TestNodes implements Iterable<TestNode> {
+
+	private final TestRun testRun;
+	
+	public TestNodes(TestRun testRun) {
+		super();
+		this.testRun = testRun;
+	}
 
 	@Override
 	public Iterator<TestNode> iterator() {
